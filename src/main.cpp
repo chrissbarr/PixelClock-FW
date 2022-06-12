a4ff2856cc2eb259a21ba02f7a41c9f5f793cae7
Chris Barr <chris@chrisbarrbuilds.com>
2022-06-12 17:28:10 +0930

    Allow setting time by serial

--- src/main.cpp
+++ src/main.cpp
@@ -214,6 +214,58 @@ void loop()
 
   loopTimeManager.idle();
 
+  if (Serial.available()) {
+    String receivedCommand = "";
+    receivedCommand = Serial.readString();
+
+    std::vector<String> substrings;
+
+    // Split the string into substrings
+    while (receivedCommand.length() > 0)
+    {
+      int index = receivedCommand.indexOf(' ');
+      if (index == -1) // No space found
+      {
+        substrings.push_back(receivedCommand);
+        break;
+      }
+      else
+      {
+        substrings.push_back(receivedCommand.substring(0, index));
+        receivedCommand = receivedCommand.substring(index+1);
+      }
+    }
+
+    if (!substrings.empty()) {
+      Serial.print("Received command: ");
+      for (const auto& str : substrings) {
+        Serial.printf("%s ", str);
+      }
+      Serial.printf("\n");
+
+      if (substrings[0] == "T") {
+        // YYYY MM DD HH MM SS
+        if (substrings.size() == 7) {
+          int year = substrings[1].toInt();
+          int month = substrings[2].toInt();
+          int day = substrings[3].toInt();
+          int hour = substrings[4].toInt();
+          int min = substrings[5].toInt();
+          int sec = substrings[6].toInt();
+
+          TimeElements time;
+          time.Year = uint8_t(CalendarYrToTm(year));
+          time.Month = uint8_t(month);
+          time.Day = uint8_t(day);
+          time.Hour = uint8_t(hour);
+          time.Minute = uint8_t(min);
+          time.Second = uint8_t(sec);
+          setTimeGlobally(makeTime(time));
+        }
+      }
+    }
+  }
+
 }
 
 
