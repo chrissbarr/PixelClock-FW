ecd0dd198c854ff166d5ff41ea084b8a2ee09cd4
Chris <chris@chrisbarrbuilds.com>
2023-12-28 23:01:10 +1030

    Fix clockface time formatting

--- src/display/effects/clockfaces.cpp
+++ src/display/effects/clockfaces.cpp
@@ -9,7 +9,7 @@
 
 canvas::Canvas ClockFace_Simple::run() {
     auto times = timeCallbackFunction();
-    std::string timestr = fmt::format("{:2d}:{:2d}", times.hour12, times.minute);
+    std::string timestr = fmt::format("{:2d}:{:02d}", times.hour12, times.minute);
     auto c = canvas::Canvas(17, 5);
     c.fill(0);
     c.showCharacters(timestr, {flm::CRGB::White}, 0, 1);
