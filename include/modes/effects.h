234bc5c2c5a79591e8a04bcca424c546adcc797d
Chris <chris@chrisbarrbuilds.com>
2024-01-18 20:58:27 +1030

    Animate effect changes

--- include/modes/effects.h
+++ include/modes/effects.h
@@ -9,6 +9,11 @@
 #include <memory>
 #include <vector>
 
+struct EffectId {
+    std::string name;
+    std::shared_ptr<DisplayEffect> ptr;
+};
+
 class Mode_Effects : public MainModeFunction {
 public:
     Mode_Effects(const canvas::Canvas& size, ButtonReferences buttons);
@@ -19,8 +24,20 @@ protected:
     void moveOutCore() override final {}
 
 private:
-    std::vector<std::shared_ptr<DisplayEffect>> effects;
+    std::vector<EffectId> effects;
     std::size_t effectIndex = 0;
+
+    int transitionDir = 0;
+    float transitionPercentage = 0.0;
+
+    enum class State {
+        Stable,
+        Transition,
+    };
+
+    uint32_t lastLoopTime = 0;
+
+    State currentState = State::Stable;
 };
 
 #endif // modes_effects_h
--- src/modes/effects.cpp
+++ src/modes/effects.cpp
@@ -17,39 +17,32 @@ using namespace printing;
 
 Mode_Effects::Mode_Effects(const canvas::Canvas& size, ButtonReferences buttons)
     : MainModeFunction("Effects", buttons) {
-    effects.push_back(std::make_unique<AudioWaterfall>(size));
-    effects.push_back(std::make_unique<VolumeGraph>(size));
-    effects.push_back(std::make_unique<VolumeDisplay>(size));
-    effects.push_back(std::make_unique<SpectrumDisplay>(size));
-    effects.push_back(std::make_unique<RandomFill>(size, 100, colourGenerator::randomHSV));
-    effects.push_back(std::make_unique<BouncingBall>(size, 100, colourGenerator::cycleHSV));
-    effects.push_back(std::make_unique<GravityFill>(size, 25, 25, colourGenerator::randomHSV));
-    effects.push_back(std::make_unique<GameOfLife>(size, 250, 5, colourGenerator::cycleHSV, false));
+    effects.push_back({"Audio Waterfall", std::make_unique<AudioWaterfall>(size)});
+    effects.push_back({"Volume Graph", std::make_unique<VolumeGraph>(size)});
+    effects.push_back({"Volume Display", std::make_unique<VolumeDisplay>(size)});
+    effects.push_back({"Spectrum Display", std::make_unique<SpectrumDisplay>(size)});
+    effects.push_back({"Random Fill", std::make_unique<RandomFill>(size, 100, colourGenerator::randomHSV)});
+    effects.push_back({"Bouncing Ball", std::make_unique<BouncingBall>(size, 100, colourGenerator::cycleHSV)});
+    effects.push_back({"Gravity Fill", std::make_unique<GravityFill>(size, 25, 25, colourGenerator::randomHSV)});
+    effects.push_back({"GoL - 1", std::make_unique<GameOfLife>(size, 250, 5, colourGenerator::cycleHSV, false)});
     auto gol = std::make_unique<GameOfLife>(size, 250, 5, colourGenerator::white, false);
     gol->setFilter(std::make_unique<RainbowWave>(1.0f, 30, RainbowWave::Direction::horizontal, true));
-    effects.push_back(std::move(gol));
+    effects.push_back({"GoL - 2", std::move(gol)});
 }
 
 void Mode_Effects::moveIntoCore() {
-    effects[effectIndex]->reset();
+    effects[effectIndex].ptr->reset();
 
-    auto cycleHandler = [this](Button2& btn) {
-        print("Switching to next effect...\n");
-        print(fmt::format("Current Effect Index: {}\n", effectIndex));
-        if (btn == buttons.left) {
-            if (effectIndex == 0) {
-                effectIndex = effects.size() - 1;
+    auto cycleHandler = [&](Button2& btn) {
+        if (currentState != State::Transition) {
+            if (btn == buttons.left) {
+                transitionDir = -1;
             } else {
-                effectIndex--;
+                transitionDir = 1;
             }
-        } else {
-            if (effectIndex == effects.size() - 1) {
-                effectIndex = 0;
-            } else {
-                effectIndex++;
-            }
+            print(fmt::format("Switching to next effect. Direction: {:+d}\n", transitionDir));
+            currentState = State::Transition;
         }
-        print(fmt::format("New Effect Index: {}\n", effectIndex));
     };
 
     buttons.left.setTapHandler(cycleHandler);
@@ -58,7 +51,61 @@ void Mode_Effects::moveIntoCore() {
 }
 
 canvas::Canvas Mode_Effects::runCore() {
-    auto c = effects[effectIndex]->run();
-    if (effects[effectIndex]->finished()) { effects[effectIndex]->reset(); }
+
+    uint32_t millisSinceLastRun = millis() - lastLoopTime;
+    float tdelta = static_cast<float>(millisSinceLastRun) / 1000;
+    lastLoopTime = millis();
+
+    canvas::Canvas c;
+    switch (currentState) {
+    case State::Stable: {
+        c = effects[effectIndex].ptr->run();
+        if (effects[effectIndex].ptr->finished()) { effects[effectIndex].ptr->reset(); }
+        break;
+    }
+    case State::Transition: {
+        print(fmt::format("State::Transition - {} - {}\n", transitionPercentage, transitionDir));
+        std::size_t effectIdxLeft = (effects.size() - 1 + effectIndex) % effects.size();
+        std::size_t effectIdxRight = (effects.size() + 1 + effectIndex) % effects.size();
+        print(fmt::format("Left Current Right - {} {} {}\n", effectIdxLeft, effectIndex, effectIdxRight));
+        canvas::Canvas cl = effects[effectIdxLeft].ptr->run();
+        canvas::Canvas cm = effects[effectIndex].ptr->run();
+        canvas::Canvas cr = effects[effectIdxRight].ptr->run();
+        canvas::Canvas cb(cm);
+        cb.fill(0);
+
+        const float transitionDuration = 0.2f; // seconds
+        transitionPercentage += (1.0f / transitionDuration) * tdelta;
+
+        auto easeInOutCubic = [](float t) -> float {
+            if (t < 0.5) {
+                return 4 * t * t * t;
+            } else {
+                float f = ((2 * t) - 2);
+                return 0.5f * f * f * f + 1;
+            }
+        };
+
+        float transitionPositionEased = easeInOutCubic(transitionPercentage);
+
+        int xOffset = static_cast<int>(std::round((transitionPositionEased * -transitionDir) * cb.getWidth()));
+
+        cb = canvas::blit(cb, cl, xOffset - cb.getWidth(), 0);
+        cb = canvas::blit(cb, cm, xOffset, 0);
+        cb = canvas::blit(cb, cr, xOffset + cb.getWidth(), 0);
+
+        if (transitionPercentage >= 1.0) {
+            effectIndex = (effects.size() + transitionDir + effectIndex) % effects.size();
+            print(fmt::format(
+                "Effect Transition Finished, New Effect = {} - {}\n", effectIndex, effects[effectIndex].name));
+            currentState = State::Stable;
+            transitionPercentage = 0;
+        }
+
+        c = cb;
+        break;
+    }
+    }
+
     return c;
 }
