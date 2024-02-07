1f7958a308ca4ac393fc7e9a7ae691d656b62989
Chris <chris@chrisbarrbuilds.com>
2024-02-07 20:02:50 +1030

    Change tests to run under desktop environment via CMake

--- test/test_canvas.cpp
+++ test/test_canvas.cpp
@@ -1,5 +1,8 @@
-#include "canvas.h"
-#include <unity.h>
+/* Project Scope */
+#include "display/canvas.h"
+
+/* Libraries */
+#include <gtest/gtest.h>
 
 using namespace canvas;
 
@@ -7,31 +10,21 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_canvas_initialised_correctly(void) {
+TEST(CanvasTestSuite, CanvasBasics) {
 
     Canvas c(10, 4);
-    TEST_ASSERT_EQUAL(10, c.getWidth());
-    TEST_ASSERT_EQUAL(4, c.getHeight());
-    TEST_ASSERT_EQUAL(40, c.getLength());
+    EXPECT_EQ(10, c.getWidth());
+    EXPECT_EQ(4, c.getHeight());
+    EXPECT_EQ(40, c.getSize());
 
-    TEST_ASSERT_TRUE(c.getXY(0, 0) == CRGB::Black);
-    TEST_ASSERT_FALSE(c.getXY(0, 0) == CRGB::Red);
+    EXPECT_EQ(c.getXY(0, 0), flm::CRGB::Black);
+    EXPECT_NE(c.getXY(0, 0), flm::CRGB::Red);
 
-    c.fill(CRGB::Red);
+    c.fill(flm::CRGB::Red);
 
-    TEST_ASSERT_TRUE(c.getXY(0, 0) == CRGB::Red);
-    TEST_ASSERT_TRUE(c.getXY(1, 1) == CRGB::Red);
+    EXPECT_EQ(c.getXY(0, 0), flm::CRGB::Red);
+    EXPECT_EQ(c.getXY(1, 1), flm::CRGB::Red);
 
-    c.setXY(1, 1, CRGB::Blue);
-    TEST_ASSERT_TRUE(c.getXY(1, 1) == CRGB::Blue);
+    c.setXY(1, 1, flm::CRGB::Blue);
+    EXPECT_EQ(c.getXY(1, 1), flm::CRGB::Blue);
 }
-
-int main(int argc, char** argv) {
-    UNITY_BEGIN();
-    RUN_TEST(test_canvas_initialised_correctly);
-    UNITY_END();
-    return 0;
-}
-
-// Satisfy FastLED external required function. Todo should be a better way.
-uint16_t XY(uint8_t x, uint8_t y) { return 0; }
